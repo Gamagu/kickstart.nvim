e1822a5c889d81099cf251d533e690c439a4409f
Niklas Opiela <niklas.opiela@sap.com>
2025-01-08 08:03:48 +0000

    stuff-changed

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,15 +3,18 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'hedyhli/outline.nvim',
-    config = function()
-      -- Example mapping to toggle outline
-      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
-      vim.keymap.set('n', '<leader>co', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
-      require('outline').setup {
-        -- Your setup opts here (leave empty to use defaults)
-      }
-    end,
-  },
+	{
+		"hedyhli/outline.nvim",
+		config = function()
+			-- Example mapping to toggle outline
+			vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+			vim.keymap.set("n", "<leader>co", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+			require("outline").setup({
+				-- Your setup opts here (leave empty to use defaults)
+			})
+		end,
+	},
+	{
+		"nvim-lua/lsp-status.nvim",
+	},
 }
